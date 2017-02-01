0df64db144751470a7c91f0ce6046b70cf0e62e7
paiatul5 <akp389@nyu.edu>
2017-01-31 21:51:56 -0500

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,25 @@
+install.packages("lubridate")
+library(lubridate)
+
+dataset <- read.csv2("household_power_consumption.txt")
+dataset <- transform(dataset,Date = dmy(Date))
+fil_by_date <- dataset[dataset[,1] > dmy("31/1/2007") & dataset[,1] < dmy("3/2/2007"),]
+fil_by_date <- transform(fil_by_date, as.Date(Date))
+fil_dt <- fil_by_date
+fil_dt[,dim(fil_dt)[2]+1] <- ymd_hms(paste(as.character(fil_dt[,1]),as.character(fil_dt[,2])))
+names(fil_dt)[10] <- "datetime"
+fil_dt <- transform(fil_dt,Global_Active_Power = as.numeric(as.character(Global_active_power)))
+fil_dt <- transform(fil_dt,Sub_metering_1 = as.numeric(as.character(Sub_metering_1)))
+fil_dt <- transform(fil_dt,Sub_metering_2 = as.numeric(as.character(Sub_metering_2)))
+fil_dt <- transform(fil_dt,Sub_metering_3 = as.numeric(as.character(Sub_metering_3)))
+fil_dt <- transform(fil_dt, Voltage = as.numeric(as.character(Voltage)), Global_reactive_power = as.numeric(as.character(Global_reactive_power)))
+png(filename = "plot4.png",height = 480, width = 480)
+par(mfrow = c(2,2))
+with(fil_dt,plot(datetime,Global_Active_Power,type="l",xlab = "",ylab = "Global Active Power"))
+with(fil_dt,plot(datetime,Voltage,type = "l"))
+with(fil_dt,plot(datetime,Sub_metering_1,xlab = "", ylab = "Energy sub metering",col="black",type="l"))
+lines(fil_dt$datetime,fil_dt$Sub_metering_2,col="red")
+lines(fil_dt$datetime,fil_dt$Sub_metering_3,col="blue")
+legend("topright",lty = 1, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"),bty = "n")
+with(fil_dt,plot(datetime,Global_reactive_power,type = "l"))
+dev.off()
