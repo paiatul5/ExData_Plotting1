63c00c4b6816d08efdab5532fa256c758a113f24
paiatul5 <akp389@nyu.edu>
2017-01-31 21:54:58 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,6 +8,6 @@ fil_by_date <- transform(fil_by_date, as.Date(Date))
 fil_dt <- fil_by_date
 fil_dt[,dim(fil_dt)[2]+1] <- ymd_hms(paste(as.character(fil_dt[,1]),as.character(fil_dt[,2])))
 fil_dt <- transform(fil_dt,Global_Active_Power = as.numeric(as.character(Global_active_power)))
-device <- png(filename = "plo1.png",height = 480,width = 480)
+device <- png(filename = "plot1.png",height = 480,width = 480)
 with(fil_dt,hist(Global_Active_Power,col="Red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)"))
 dev.off()
